7c3f2575f5405052b6b604921449efcf04b7a1d8
Gabriel <gabriel.mg42@outlook.com>
2022-03-09 00:25:49 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,34 @@
 import Pagination from "assets/components/Pagination";
+import MovieCard from "assets/components/MovieCard";
 
 function Listing (){
     return (
+        <>
         <Pagination />
+
+    <div className="container">
+        <div className="row">
+            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
+                <MovieCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
+                <MovieCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
+                <MovieCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
+                <MovieCard />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
+                <MovieCard />
+            </div>
+        </div>
+    </div>
+
+        
+
+        </> 
     );
 }
 
