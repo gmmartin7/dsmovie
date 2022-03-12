9ae0de43a609d6cc29f42cd34cce3111bbc55f49
Gabriel <gabriel.mg42@outlook.com>
2022-03-12 10:16:33 -0300

    Save Score

--- backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -1,42 +1,33 @@
-package com.devsuperior.dsmovie.entities;
+package com.devsuperior.dsmovie.dto;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.devsuperior.dsmovie.entities.Movie;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tb_movie")
-public class Movie {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+public class MovieDTO {
 	private Long id;
 	private String title;
 	private Double Score;
 	private Integer count;
 	private String image;
 	
-	@OneToMany(mappedBy = "id.movie")
-	private Set<Score> scores = new HashSet<>();
-	
-	public Movie() {
+	public MovieDTO() {
 		
 	}
 
-	public Movie(Long id, String title, Double score, Integer count, String image) {
-		super();
+	public MovieDTO(Long id, String title, Double score, Integer count, String image) {
 		this.id = id;
 		this.title = title;
 		Score = score;
 		this.count = count;
 		this.image = image;
 	}
+	
+	public MovieDTO(Movie movie) {
+		id = movie.getId();
+		title = movie.getTitle();
+		Score = movie.getScore();
+		count = movie.getCount();
+		image = movie.getImage();
+}
 
 	public Long getId() {
 		return id;
@@ -77,12 +68,5 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
-
-	public Set<Score> getScores() {
-		return scores;
-	}
-
-	
-	
 	
 }
