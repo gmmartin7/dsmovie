31f447a730537e84b98e192ed67ddce936576149
Gabriel <gabriel.mg42@outlook.com>
2022-03-12 21:58:35 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,35 +1,49 @@
 import Pagination from "assets/components/Pagination";
 import MovieCard from "assets/components/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
-function Listing (){
-    return (
-        <>
-        <Pagination />
+function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
-    <div className="container">
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
+
+  return (
+    <>
+      <Pagination />
+
+      <div className="container">
         <div className="row">
-            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
-                <MovieCard />
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-x1-3 mb=3">
+              <MovieCard movie={movie} />
             </div>
-            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-x1-3 mb=3">
-                <MovieCard />
-            </div>
+          ))}
         </div>
-    </div>
-
-        
-
-        </> 
-    );
+      </div>
+    </>
+  );
 }
 
 export default Listing;
